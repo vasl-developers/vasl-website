d098d1fea4b68c3d7027e41616739cdc53049eb0
Sam Tyson <styson@gmail.com>
2016-03-05 14:02:58 -0600

    added scenario setup content builder via jekyll plugin

---
+++ _plugins/setup_generator.rb
@@ -0,0 +1,57 @@
+require 'jekyll/document'
+
+module Jekyll
+
+  # Recover from strange exception when starting server without --auto
+  class SitemapFile < StaticFile
+    def write(dest)
+      true
+    end
+  end
+
+  class SitemapGenerator < Generator
+    priority :lowest
+
+    # Config defaults
+    FILE_NAME = "/setups/scen.html"
+
+    # Goes through setup files and generates listing
+    def generate(site)
+      # Configuration
+      sitemap_config = site.config['sitemap'] || {}
+
+      # create destination directory if it doesn't exist yet
+      Dir::mkdir(site.dest) if !File.directory? site.dest
+      $fileHtml = File.new(File.join(site.dest, FILE_NAME), "w+")
+
+      traverse(File.join(site.dest, "/setups"))
+
+      $fileHtml.close()
+
+      # Keep the file from being cleaned by Jekyll
+      site.static_files << Jekyll::SitemapFile.new(site, site.dest, "/", FILE_NAME)
+    end
+
+    def process(path)
+      if File.directory?(path)
+        dir = File.basename(path)
+        level = path.count('/') - 4
+        h = "h" + level.to_s
+        $fileHtml.puts "<"+h+">" + dir + "</"+h+">" if dir != "Enhanced"
+      else
+        $fileHtml.puts "<p><a href='"+path+"'>" + File.basename(path, ".vsav") + "</a></p>"
+      end
+    end
+
+    def traverse(path='.')
+      Dir.entries(path).each do |name|
+        next  if name == '.' or name == '..'
+
+        path2 = path + '/' + name
+        process(path2) if File.basename(path2) != "scen.html"
+
+        traverse(path2)  if File.ftype(path2) == "directory"
+      end
+    end
+  end
+end
